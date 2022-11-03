c8a29ebb5f06fa7a91b6603519858e70b4c5d0df
Luciano Rocha <luciano@cloudinsider.com.br>
2022-11-03 12:52:17 -0300

    feat: lista de items na pagina dashboard

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,43 @@
-import { Link } from "react-router-dom";
+import { useCallback, useState } from "react";
+
+
 
 export const Dashboard = () => {
+
+    const [lista, setLista] = useState<string[]>(["teste1", "teste2", "teste3"]);
+
+    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if (e.key === 'Enter') {
+            if (e.currentTarget.value.trim().length === 0) return;
+
+            const value = e.currentTarget.value;
+
+            e.currentTarget.value = '';
+
+            setLista((oldLista) => {
+
+                if (oldLista.includes(value)) return oldLista;
+
+                return [...oldLista, value];
+            });
+        }
+
+    }, [])
+
     return (
         <div>
-            <p>Dashboard</p>
-            <Link to="/login">Login</ Link>
+            <p>Lista</p>
+
+            <input
+                onKeyDown={handleInputKeyDown}
+
+            />
+
+            <ul>
+                {lista.map((value) => {
+                    return <li key={value}>{value}</li>;
+                })}
+            </ul>
         </div>
     );
 }
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,25 +1,67 @@
-import { useState } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 
 export const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleEntrar = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
+
+    const emailLength = useMemo(() => {
+        // console.log('executou')
+        return email.length * 1000;
+    }, [email.length]);
+
+    // useEffect(() => {
+    //     if (window.confirm('você é homem?')) {
+    //         console.log('Homem')
+    //     } else {
+    //         console.log('Mulher')
+    //     }
+    // }, []);
+
+    // useEffect(() => {
+    //     console.log(email)
+    // }, [email]);
+
+    // useEffect(() => {
+    //     console.log(password)
+    // }, [password]);
+
+    // const handleEntrar = () => {
+    //     console.log(email)
+    //     console.log(password)
+    // }
+
+    const handleEntrar = useCallback(() => {
         console.log(email)
         console.log(password)
-    }
+    }, [email, password]);
+
+
 
     return (
         <div>
             <form>
+                <p>seu e-mail tem {emailLength} caracteres.</p>
+
                 <label>
                     <span>E-mail</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)} />
+                    <input
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    />
+
                 </label>
 
                 <label>
                     <span>Senha</span>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <input
+                        type="password"
+                        value={password}
+                        ref={inputPasswordRef}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                 </label>
 
                 <button type="button" onClick={handleEntrar}>
